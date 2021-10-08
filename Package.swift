548f58024885706bbf899f7393f6591082f5ca5f
Baran Karaoğuz <baran.karaoguz@ogr.sakarya.edu.tr>
2021-10-08 17:05:09 +0300

    Update Package.swift
    Renamed Package Name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Charts",
+    name: "SwiftUICharts",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
